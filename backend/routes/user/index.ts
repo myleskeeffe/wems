6be837272ec239ed9cac1ed4bd8aa7535c613330
Myles K <11828103+myleskeeffe@users.noreply.github.com>
2021-05-05 10:04:52 +0800

    DB Setup & Refactor

--- backend/routes/user/index.ts
+++ backend/routes/user/index.ts
@@ -1,6 +1,8 @@
 const models = require('express').Router();
 const list = require('./list');
 
+let User = require("./models/User")(sequelize, DataTypes).User;
+
 models.get('/', list);
 
 module.exports = models;
