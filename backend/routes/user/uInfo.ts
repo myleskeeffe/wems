ba5474e131f33949b81841c0c13dd2ec925e4ffd
Myles K <11828103+myleskeeffe@users.noreply.github.com>
2021-05-06 10:08:26 +0800

    Update uInfo

--- backend/routes/user/uInfo.ts
+++ backend/routes/user/uInfo.ts
@@ -1,21 +1,17 @@
 let findUser = require('../../utils/auth/findUser');
 
 module.exports = (req: any, res: any) => {
-    if (!req.params.) {
+    if (!req.params.id) {
       return(res.json({error: 'Please fill all required fields.'}))
     }
 
-    let fName = req.body.fName
-    let lName = req.body.lName
-    let uName = req.body.uName
-    let phoneNumber = req.body.phoneNumber
-    let email = req.body.email
-    let password = req.body.password
-    try {
-      createUser(fName, lName, uName, phoneNumber, email, password);
-      res.json({ success: "User Created: " + uName })
-    }
-    catch(err) {
-      res.json({ error: err })
-    }
+    let id = req.params.id
+
+    findUser(id).then(function (userData: any) {
+      res.json(userData)
+    }).catch(function (error: any) {
+      console.log(error)
+      res.json({error: 'We encoutered an error processing that request.'})
+    })
+
 };
