729af837c16694a774c74f8b0e9a377c87058eaa
Myles K <11828103+myleskeeffe@users.noreply.github.com>
2021-05-17 13:42:10 +0800

    Reduce Complexity (Can be added back later)

--- backend/models/Guardian.ts
+++
@@ -1,9 +0,0 @@
-export const GuardianModel = function(sequelize: any, DataTypes: any){
-  return sequelize.define('Guardian', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-  })
-}
